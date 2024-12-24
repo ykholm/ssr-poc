64ea146e123c36ac42635da96c9de86bb663bd74
Yevhenii Kholmanskykh <ykholman@duosecurity.com>
2024-12-24 09:47:48 +0200

    add compression

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,27 @@
 import express from 'express'
 import fs from 'fs'
 import path from 'path'
+import compression from 'compression'
 import {renderHtml} from "./render";
 
 const server = express()
+
+// Enable compression middleware
+server.use(compression())
 
 server.set('view engine', 'ejs')
 server.set('views', path.join(__dirname, 'views'))
 
 // Static middleware with error logging
-server.use('/', express.static(path.join(__dirname, 'static')))
+server.use('/', express.static(path.join(__dirname, 'static'), {
+    maxAge: '1y', // Cache static assets for one year
+    etag: false, // Disable etag headers if using maxAge
+    setHeaders: (res, path) => {
+        if (path.endsWith('.html')) {
+            res.setHeader('Cache-Control', 'no-cache')
+        }
+    }
+}))
 
 let jsAssets: Map<string, string>;
 try {
@@ -23,7 +35,7 @@ try {
     process.exit(1) // Exit gracefully if assets fail to load
 }
 
-server.get('/demo', (req, res) => {
+server.get('*', (req, res) => {
     try {
         const component = renderHtml(req.url);
         res.render('index', { assets: jsAssets, component })
